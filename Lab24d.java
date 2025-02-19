6b1565bef73f54027abe38e9111d92665a4e0004
mRaines08 <mallory.raines@stu.daviess.kyschools.us>
2025-02-19 15:50:14 +0000

    classWordAndHomework21925

--- Lab24d.java
+++ Lab24d.java
@@ -14,6 +14,29 @@ public class Lab24d
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
 
+		int num = file.nextInt();
+		System.out.println(file.nextLine());
+
+		for(int i = 0; i < num; i++)
+		{
+			TicTacToe board = new TicTacToe(file.nextLine());
+			System.out.println("board " + (i+1) + board);
+		}
+
+		
+		
+		//read how many times you should read
+			//create an int that will except the numbr of times you should read
+			// use next line after you read the int
+			//use a traditional for loop
+
+			//file.nextLine() reads all the way to the end of a line as a string
+		
+		//Tictactoe only wants File.nextLine()
+
+		//make a tictactoe object for each game (row) in the file
+
+		//
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -13,44 +13,63 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
-
+		mat = new char[3][3];
 	}
 
 	public TicTacToe(String game)
 	{
+		//row major order will happen here
+		//use charAt()
+		mat = new char[3][3];
 
+		int pos = 0;
 
-
-
+		for(int r = 0; r < mat.length; r++)
+			for(int c = 0; c < mat[r].length; c++)
+			{
+				mat[r][c] = game.charAt(pos);
+				pos++;
+			}
 	}
 
 	public String getWinner()
 	{
 
-
-
-
-
-
-
-
-
-
-
-
-		return "";
+		// if the first letter is the same as the second letter and the second letter is the same as the thrid letter then that letter wins
+		// do same for vertical and diagonal
+		// else tie
+		if(mat[0][0] == mat[0][1] && mat[0][1] == mat[0][2])
+			return "" + mat[0][0] + " wins horizontally!";
+		else if(mat[1][0] == mat[1][1] && mat[1][1] == mat[1][2])
+			return "" + mat[1][0] + " wins horizontally!";
+		else if(mat[2][0] == mat[2][1] && mat[2][1] == mat[2][2])
+			return "" + mat[2][0] + " wins horizontally!";
+		else if(mat[0][0] == mat[1][0] && mat[1][0] == mat[2][0])
+			return "" + mat[0][0] + " wins vertically!";
+		else if(mat[0][1] == mat[1][1] && mat[1][1] == mat[2][1])
+			return "" + mat[0][1] + " wins vertically!";
+		else if(mat[0][2] == mat[1][2] && mat[1][2] == mat[2][2])
+			return "" + mat[0][2] + " wins vertically!";
+		else if(mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2])
+			return "" + mat[0][0] + " wins diagonally!";
+		else if(mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0])
+			return "" + mat[0][2] + " wins diagonally!";
+		else
+			return "cat's game - no winner!";
 	}
 
 	public String toString()
 	{
 		String output="";
 
+		//prints board use row major order
+		for(char[] row : mat)
+		{
+			output += "\n";
+			for(char c : row)
+				output += " " + c;
+		}
 
-
-
-
-
-		return output+"\n\n";
+		return output + "\n" + getWinner() + "\n\n";
 	}
 }
